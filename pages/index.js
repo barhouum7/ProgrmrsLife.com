f49cd7674ad42f12e3a08bbcd1ea2a484e206e14
barhouum7 <bb.med2000@gmail.com>
2023-03-02 10:46:33 +0100

    Fix: Looking throught a fix

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import { PostCard, Categories, PostWidget} from '../components'
 import { getPosts } from '../services'
-// import { Header } from '../sections';
 
 
 export default function Home ({ posts }) {
   return (
-    <>
-      {/* <Header /> */}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 mt-20">
         <div className='lg:col-span-8 col-span-1'>
         {posts.map((post, index) => <PostCard key={post.node.title} post={post.node} />)}
@@ -22,15 +19,15 @@ export default function Home ({ posts }) {
           </div>
         </div>
       </div>
-    </>
-  )
+  );
 }
 
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
     props: {
       posts
     }
-  }
+  };
 }
