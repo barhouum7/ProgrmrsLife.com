bc6100f48057c7920842703e0153e82af12d6217
barhouum7 <bb.med2000@gmail.com>
2023-04-10 09:27:24 +0100

    2 Kbar Action hooks added + Refactor code

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,13 @@ import {
   KBarResults,
   useMatches,
   createAction,
+  useRegisterActions,
   ActionId,
   ActionImpl,
   NO_GROUP
 } from "kbar";
+import usePostsActions from "../hooks/usePostsActions";
+import useCategoriesActions from "../hooks/useCategoriesActions";
 import useThemeActions from "../hooks/useThemeActions";
 
 import '../styles/globals.scss'
@@ -85,7 +88,7 @@ const ResultItem = React.forwardRef(
     {
       action = ActionImpl.NO_ACTION,
       active,
-      currentRootActionId = ActionId.NO_ACTION,
+      currentRootActionId,
     },
     ref
   ) => {
@@ -138,14 +141,15 @@ const ResultItem = React.forwardRef(
                 ancestors.map((ancestor) => (
                   <React.Fragment key={ancestor.id}>
                     <span
-                      style={{
-                        opacity: 0.5,
-                        marginRight: 8,
-                        background: "rgba(0 0 0 / .1)",
-                        padding: "4px 6px",
-                        borderRadius: "4px",
-                        color: "rgba(255, 255, 255, 0.7)",
-                      }}
+                      // style={{
+                      //   opacity: 0.5,
+                      //   marginRight: 8,
+                      //   background: "rgba(0 0 0 / .1)",
+                      //   padding: "4px 6px",
+                      //   borderRadius: "4px",
+                      //   color: "rgba(255, 255, 255, 0.7)",
+                      // }}
+                      className="mr-2 bg-gray-800 bg-opacity-10 dark:bg-gray-300 dark:bg-opacity-10 text-gray-900 dark:text-gray-100 rounded px-2 py-1"
                     >
                       {ancestor.name}
                     </span>
@@ -194,6 +198,8 @@ const ResultItem = React.forwardRef(
 );
 
 function CommandBar() {
+  usePostsActions();
+  useCategoriesActions();
   useThemeActions();
   return (
     <KBarPortal> 
@@ -246,17 +252,7 @@ const actions = [
       window.location.href = "/AboutUs"
     },
     subtitle: "Go to the about page",
-  },
-  {
-    id: "contact",
-    name: "Contact",
-    shortcut: ["c"],
-    section: "Navigation",
-    keywords: ["contact, contact us, contact us page, contact page, email, email us, email us page, email page"],
-    perform: () => {
-      window.location.href = "/ContactUs"
-    },
-    subtitle: "Go to the contact page",
+    icon: <AboutIcon />,
   },
   {
     id: "services",
@@ -268,101 +264,8 @@ const actions = [
       window.location.href = "/Services"
     },
     subtitle: "Go to the services page",
+    icon: <ServicesIcon />,
   },
-  {
-    id: "free-online-resources",
-    name: "Free Online Resources",
-    shortcut: ["f", "r"],
-    keywords: ["free, free online resources, free online resources page, free resources, free resources page, free online, free online page, free page, online resources, online resources page, online, online page, resources, resources page"],
-    section: "Categories",
-    perform: () => {
-      window.location.href = "/category/free-online-resources"
-    },
-    subtitle: "Go to the free online resources category page",
-  },
-  {
-    id: "tech-guides",
-    name: "Tech Guides",
-    shortcut: ["t", "g"],
-    keywords: ["tech, tech guides, tech guides page, guides, guides page, tech page, guides page"],
-    section: "Categories",
-    perform: () => {
-      window.location.href = "/category/tech-guides"
-    },
-    subtitle: "Go to the tech guides category page",
-  },
-  {
-    id: "web-dev",
-    name: "Web Development",
-    shortcut: ["w", "d"],
-    keywords: ["web, web dev, web development, web development page, web dev page, development, development page, dev, dev page"],
-    section: "Categories",
-    perform: () => {
-      window.location.href = "/category/web-dev"
-    },
-    subtitle: "Go to the web development category page",
-  },
-  {
-    id: "tips-and-tricks",
-    name: "Tips & Tricks",
-    shortcut: ["t"],
-    keywords: ["tips, tricks, tips and tricks, tips & tricks, tips and tricks page, tips & tricks page, tips page, tricks page"],
-    section: "Categories",
-    perform: () => {
-      window.location.href = "/category/tips-and-tricks"
-    },
-    subtitle: "Go to the tips and tricks category page",
-  },
-  {
-    id: "linux-sys-admin",
-    name: "Linux Sys Admin",
-    shortcut: ["l", "a"],
-    keywords: ["linux, linux sys admin, linux sys admin page, linux system admin, linux system admin page, linux system administration, linux system administration page, linux administration, linux administration page, linux admin, linux admin page, linux administration, linux administration page, linux sys, linux sys page, linux system, linux system page, linux system administration, linux system administration page, linux administration, linux administration page, linux admin, linux admin page, linux administration, linux administration page, sys admin, sys admin page, system admin, system admin page, system administration, system administration page, administration, administration page, admin, admin page, administration, administration page, sys, sys page, system, system page, system administration, system administration page, administration, administration page, admin, admin page, administration, administration page"],
-    section: "Categories",
-    perform: () => {
-      window.location.href = "/category/linux-sys-admin"
-    },
-    subtitle: "Go to the linux sys admin category page",
-  },
-  {
-    id: "news",
-    name: "News",
-    shortcut: ["n"],
-    keywords: ["news, news page"],
-    section: "Categories",
-    perform: () => {
-      window.location.href = "/category/news"
-    },
-    subtitle: "Go to the news category page",
-  },
-  {
-    id: "events",
-    name: "Events",
-    shortcut: ["e"],
-    keywords: ["events, events page"],
-    section: "Categories",
-    perform: () => {
-      window.location.href = "/category/events"
-    },
-    subtitle: "Go to the events category page",
-  },
-  {
-    id: "twitterAction",
-    name: "Twitter",
-    shortcut: ["g", "t"],
-    keywords: "social contact dm",
-    section: "Social Media",
-    perform: () => window.open("https://twitter.com/mindh4q3rr", "_blank"),
-    subtitle: "Go to our Twitter profile",
-  },
-  createAction({
-    name: "Github",
-    shortcut: ["g"],
-    keywords: "sourcecode",
-    section: "Social Media",
-    perform: () => window.open("https://github.com/barhouum7", "_blank"),
-    subtitle: "Go to our Github profile",
-  }),
 ]
 
   return (
@@ -388,11 +291,27 @@ export default MyApp
 
 function HomeIcon() {
   return (
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-8 h-8 -ml-1 -mr-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
       <path
         d="m19.681 10.406-7.09-6.179a.924.924 0 0 0-1.214.002l-7.06 6.179c-.642.561-.244 1.618.608 1.618.51 0 .924.414.924.924v5.395c0 .51.414.923.923.923h3.236V14.54c0-.289.234-.522.522-.522h2.94c.288 0 .522.233.522.522v4.728h3.073c.51 0 .924-.413.924-.923V12.95c0-.51.413-.924.923-.924h.163c.853 0 1.25-1.059.606-1.62Z"
-        fill="currentColor" className="text-gray-500 dark:text-gray-400"
+        fill="currentColor"
       />
     </svg>
   );
+}
+
+function AboutIcon () {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <path fill="currentColor" d="M12 22c-5.523 0-10-4.477-10-10s4.477-10 10-10 10 4.477 10 10-4.477 10-10 10zm0-2c4.411 0 8-3.589 8-8s-3.589-8-8-8-8 3.589-8 8 3.589 8 8 8zM11 7h2v6h-2zm0 8h2v2h-2z"/>
+    </svg>
+  )
+}
+
+function ServicesIcon () {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 -ml-1 -mr-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+      <path d="M5 5H19V19H5V5ZM7 7V17H17V7H7ZM8.5 8.5H11.5V10.5H8.5V8.5ZM12.5 8.5H15.5V10.5H12.5V8.5ZM8.5 11.5H11.5V13.5H8.5V11.5ZM12.5 11.5H15.5V13.5H12.5V11.5ZM8.5 14.5H11.5V16.5H8.5V14.5ZM12.5 14.5H15.5V16.5H12.5V14.5Z" fill="currentColor"/>
+    </svg>
+  )
 }
