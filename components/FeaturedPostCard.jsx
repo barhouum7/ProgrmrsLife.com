7e6bae33315e7fdcf00f4d2346efe1dcd7465782
barhouum7 <bb.med2000@gmail.com>
2023-04-11 02:07:57 +0100

    Refactor code

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 
 const FeaturedPostCard = ({ post }) => {
     return (
-        <div className="relative h-72">
-            <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
+        <div className="relative h-72 shadow-lg cursor-pointer hover:opacity-80 transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl hover:z-50 hover:rounded-lg">
+            <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
             <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
         <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
         <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
