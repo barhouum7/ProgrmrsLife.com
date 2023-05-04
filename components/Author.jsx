4ff2fbb7b7543473751c9bcb540c467dbeb70818
barhouum7 <bb.med2000@gmail.com>
2023-05-04 23:46:47 +0100

    Small Fix

--- components/Author.jsx
+++ components/Author.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Author = ({ author }) => {
   return (
-        <div className='text-center z-30 mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20 dark:bg-opacity-30 rounded-lg shadow-xl lg:p-8 transition duration-700 ease-in-out transform hover:shadow-indigo-500/40 hover:shadow-2xl'>
+        <div className='text-center z-30 mt-20 mb-8 p-12 relative bg-black bg-opacity-20 dark:bg-opacity-30 rounded-lg shadow-xl lg:p-8 transition duration-700 ease-in-out transform hover:shadow-indigo-500/40 hover:shadow-2xl'>
             <div className="absolute -z-20 inset-0 bg-white opacity-0 rounded-lg shadow-2xl animated w-2 h-15"></div>
             <div className="relative z-30 p-6">
-                <div className='animate-spin-slow animate-[spin_3s_ease-in-out_infinite] hover:animate-none transition duration-700 ease-in-out flex justify-center absolute right-20 left-20 -top-20'>
+                <div className='transition duration-700 ease-in-out flex justify-center absolute right-20 left-20 -top-20'>
                     <img
                         src={author.photo.url}
                         alt={author.name}
