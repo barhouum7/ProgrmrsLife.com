6c9a9146ad5c666774b811070f8f3698b281b323
barhouum7 <bb.med2000@gmail.com>
2023-05-18 11:35:52 +0100

    Fixing fetching posts limit

--- services/index.js
+++ services/index.js
@@ -16,7 +16,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 export const getPosts = async () => {
     const query = gql`
     query MyQuery {
-        postsConnection {
+        postsConnection(first: 100) {
           edges {
             node {
               author {
