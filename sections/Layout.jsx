56385cc8418b2b9684518983215e1b0e1621b156
barhouum7 <bb.med2000@gmail.com>
2023-05-31 00:12:55 +0100

    Feat: AWeber Script for push notifications

--- sections/Layout.jsx
+++ sections/Layout.jsx
@@ -154,6 +154,19 @@ const Layout = ({ children }) => {
           data-website-uuid="6be0f015-e759-4ffd-8346-ebb290ddbdf9"
           async
         />
+        <Script async src="https://assets.aweber-static.com/aweberjs/aweber.js" />
+          <Script>
+              {`
+                var AWeber = window.AWeber || [];
+                AWeber.push(function() {
+                    AWeber.WebPush.init(
+                        'BPWeSrUv87tnPkdoNOpUlMTllSqJ-fyJCJlYT2eeU8M6fBSFwO4FfQWcU83SEte8wowpIdhAH2P19HJDkUuAmAc',
+                        'a8f015e0-3041-4a1c-be7f-e8817369ba07',
+                        '9b0f384d-00c7-46f4-b678-2d996e3f2e4a'
+                    );
+                });
+              `}
+          </Script>
     </>
   );
 };
