3fed8de37745fe64af87195c70e91890896a39a2
barhouum7 <bb.med2000@gmail.com>
2023-08-03 12:24:59 +0100

    Aweber fix

--- pages/_document.js
+++ pages/_document.js
@@ -16,9 +16,27 @@ class MyDocument extends Document {
         <Script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=64cb74163aa29300123c3d5b&product=inline-reaction-buttons&source=platform" async="async" />
         </Head>
         <body>
-          <Script async src="https://assets.aweber-static.com/aweberjs/aweber.js" />
           <Main />
           <NextScript />
+          {/* Paste the AWeber script below */}
+          <script
+            async
+            src="https://assets.aweber-static.com/aweberjs/aweber.js"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var AWeber = window.AWeber || [];
+                AWeber.push(function() {
+                  AWeber.WebPush.init(
+                    'BPWeSrUv87tnPkdoNOpUlMTllSqJ-fyJCJlYT2eeU8M6fBSFwO4FfQWcU83SEte8wowpIdhAH2P19HJDkUuAmAc',
+                    'a8f015e0-3041-4a1c-be7f-e8817369ba07',
+                    '9b0f384d-00c7-46f4-b678-2d996e3f2e4a'
+                  );
+                });
+              `,
+            }}
+          ></script>
           
           <Script
             type="text/javascript"
