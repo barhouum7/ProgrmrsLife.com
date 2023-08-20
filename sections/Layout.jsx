0e273d996d79864960dd41fc72ae329e36e60204
barhouum7 <bb.med2000@gmail.com>
2023-08-20 03:09:02 +0100

    Feat/Website accessibility

--- sections/Layout.jsx
+++ sections/Layout.jsx
@@ -50,6 +50,33 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Head>
+      <script>
+      {`
+          (function(d){
+            var s = d.createElement("script");
+            /* uncomment the following line to override default position*/
+            s.setAttribute("data-position", 2);
+            /* uncomment the following line to override default size (values: small, large)*/
+            /* s.setAttribute("data-size", "small");*/
+            /* uncomment the following line to override default language (e.g., fr, de, es, he, nl, etc.)*/
+            s.setAttribute("data-language", "language");
+            /* uncomment the following line to override color set via widget (e.g., #053f67)*/
+            /* s.setAttribute("data-color", "#053e67");*/
+            /* uncomment the following line to override type set via widget (1=person, 2=chair, 3=eye, 4=text)*/
+            /* s.setAttribute("data-type", "1");*/
+            /* s.setAttribute("data-statement_text:", "Our Accessibility Statement");*/
+            /* s.setAttribute("data-statement_url", "http://www.example.com/accessibility")";*/
+            /* uncomment the following line to override support on mobile devices*/
+            s.setAttribute("data-mobile", true);
+            /* uncomment the following line to set custom trigger action for accessibility menu*/
+            /* s.setAttribute("data-trigger", "triggerId")*/
+            s.setAttribute("data-account", "qvONRLOSyv");
+            s.setAttribute("src", "https://cdn.userway.org/widget.js");
+            (d.body || d.head).appendChild(s);
+          })(document)
+      `}
+      </script>
+        <noscript>Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a></noscript>
         <title>ProgrammersLife™</title>
         <link rel="icon" href="/imgs/favicon.svg" />
         <meta name="description" content="ProgrammersLife™ is a blog about programming, web development, and tech in general. We write about the latest technologies, frameworks, and libraries. We also write about the latest news in the tech world." />
