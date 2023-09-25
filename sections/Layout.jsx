7550ba3680b1c778285201c1108abb249ec14b70
barhouum7 <bb.med2000@gmail.com>
2023-09-25 13:57:33 +0100

    fix

--- sections/Layout.jsx
+++ sections/Layout.jsx
@@ -46,32 +46,32 @@ const Layout = ({ children }) => {
     <div>
       <Head>
         {/* UserWay Script */}
-      <script>
+      <Script>
       {`
           (function(d){
-            var s = d.createElement("script");
+            var s = d.createElement('script');
             /* uncomment the following line to override default position*/
-            s.setAttribute("data-position", 2);
+            s.setAttribute('data-position', 2);
             /* uncomment the following line to override default size (values: small, large)*/
-            /* s.setAttribute("data-size", "small");*/
+            /* s.setAttribute('data-size', 'small');*/
             /* uncomment the following line to override default language (e.g., fr, de, es, he, nl, etc.)*/
-            s.setAttribute("data-language", "language");
+            s.setAttribute('data-language', 'language');
             /* uncomment the following line to override color set via widget (e.g., #053f67)*/
-            /* s.setAttribute("data-color", "#053e67");*/
+            /* s.setAttribute('data-color', '#053e67');*/
             /* uncomment the following line to override type set via widget (1=person, 2=chair, 3=eye, 4=text)*/
-            /* s.setAttribute("data-type", "1");*/
-            /* s.setAttribute("data-statement_text:", "Our Accessibility Statement");*/
-            /* s.setAttribute("data-statement_url", "http://www.example.com/accessibility")";*/
+            /* s.setAttribute('data-type', '1');*/
+            /* s.setAttribute('data-statement_text:', 'Our Accessibility Statement');*/
+            /* s.setAttribute('data-statement_url', 'http://www.example.com/accessibility');*/
             /* uncomment the following line to override support on mobile devices*/
-            s.setAttribute("data-mobile", true);
+            s.setAttribute('data-mobile', true);
             /* uncomment the following line to set custom trigger action for accessibility menu*/
-            /* s.setAttribute("data-trigger", "triggerId")*/
-            s.setAttribute("data-account", "qvONRLOSyv");
-            s.setAttribute("src", "https://cdn.userway.org/widget.js");
+            /* s.setAttribute('data-trigger', 'triggerId')*/
+            s.setAttribute('data-account', 'qvONRLOSyv');
+            s.setAttribute('src', 'https://cdn.userway.org/widget.js');
             (d.body || d.head).appendChild(s);
           })(document)
       `}
-      </script>
+      </Script>
         <noscript>Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a></noscript>
         <title>ProgrammersLife - Your Guide to Web Development, Tips & Tricks and Tech News</title>
         <link rel="icon" href="/imgs/favicon.svg" />
