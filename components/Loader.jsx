8c2cab2805aa986bbcf01b85485cce14319d584a
barhouum7 <bb.med2000@gmail.com>
2024-09-10 07:11:24 +0100

    Replace GridLoader with HashLoader in Loader component for better visibility

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,14 +1,14 @@
-import GridLoader from "react-spinners/GridLoader";
+import HashLoader from "react-spinners/HashLoader";
 
 const Loader = ({
   loading
 }) => {
   return (
     <div className="loader-container">
-      <GridLoader
-        color={"white"}
+      <HashLoader
+        color={"#4F46E5"} // Indigo-600 color for better visibility
         loading={loading}
-        size={30}
+        size={90}
         aria-label="Loading spinner"
         data-testid="loader"
       />
