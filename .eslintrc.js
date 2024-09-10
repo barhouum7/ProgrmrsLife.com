680b72562b475368480b31ad97b120f6c1c57b51
barhouum7 <bb.med2000@gmail.com>
2024-09-10 07:05:17 +0100

    Added typescript + other dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    // Add custom rules here
+  },
+};
