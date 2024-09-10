6560e0f3c99197fd707d9f932d936cd00a628efd
barhouum7 <bb.med2000@gmail.com>
2024-09-10 19:37:21 +0100

    Added ErrorBoundary + SuspenseLoader components

---
+++ components/SuspenseLoader.jsx
@@ -0,0 +1,24 @@
+import BarLoader from "react-spinners/BarLoader";
+import { useTheme } from "next-themes";
+
+const SuspenseLoader = () => {
+  const { theme } = useTheme();
+
+  // Determine color based on theme
+  const loaderColor = theme === 'dark' ? "#60A5FA" : "#BFDBFE"; // Light blue for dark mode, very light blue for light mode
+
+  return (
+    <div className="fixed top-0 left-0 w-screen z-50">
+      <BarLoader
+        color={loaderColor}
+        loading={true}
+        width="100%"
+        height={4}
+        aria-label="Loading bar"
+        data-testid="loader"
+      />
+    </div>
+  );
+};
+
+export default SuspenseLoader;
