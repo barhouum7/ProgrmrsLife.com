7aff17cb9c1550702c03cad777a1d10efce8ec88
barhouum7 <bb.med2000@gmail.com>
2024-09-14 12:22:33 +0100

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,28 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
+    // 'eslint:recommended',
     'plugin:react/recommended',
+    // Remove stricter configs temporarily
   ],
-  rules: {
-    // Add custom rules here
-  },
-};
+  // ...
+}
+
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   extends: [
+//     'next/core-web-vitals',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:react/recommended',
+//   ],
+//   rules: {
+//     'react/react-in-jsx-scope': 'off',
+//     'react/prop-types': 'error',
+//     'react/no-unescaped-entities': 'error',
+//     'react-hooks/exhaustive-deps': 'warn',
+//     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+//     '@next/next/no-img-element': 'warn',
+//     '@next/next/no-html-link-for-pages': 'error',
+//     '@next/next/inline-script-id': 'error',
+//   },
+// };
