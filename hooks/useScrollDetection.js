7c61dccad92d6642c946c88522b4c907635ddb5b
barhouum7 <bb.med2000@gmail.com>
2024-11-19 03:09:05 +0100

    Feat: Added useScrollDetection hook

---
+++ hooks/useScrollDetection.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+export const useScrollDetection = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsScrolled(scrollPosition > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return isScrolled;
+};
