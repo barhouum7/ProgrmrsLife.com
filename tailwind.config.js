d1fe3eae4b50dd360426abca6a741e6bc4539a18
barhouum7 <bb.med2000@gmail.com>
2024-11-19 05:01:48 +0100

    Feat: Added new animation configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,47 @@ module.exports = {
       animation: {
         'spin': 'spin 6s linear infinite',
         'ping-slow': 'ping 4s cubic-bezier(0, 0, 0.2, 1) infinite',
-      }
+        'fadeIn': 'fadeIn 0.2s ease-in-out',
+        'gradient': 'gradient 3s linear infinite',
+        'gradient-xy': 'gradient-xy 15s ease infinite',
+        'gradient-x': 'gradient-x 15s ease infinite',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        gradient: {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': '0% 50%'
+          },
+          '50%': {
+            'background-size': '400% 400%',
+            'background-position': '100% 50%'
+          },
+        },
+        'gradient-xy': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
     }
   },
   plugins: [
